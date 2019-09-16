356c819030a32e1ca7de6761db893d300b926826
Miguel Angel Ortega Flores <miguelortega.20@outlook.com>
2019-09-15 23:58:40 -0500

    programa base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practica1MetodologiaProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa");
+        }
+    }
+}
